aae5b273e8b3076e80ad4ec35940ab0be9fb0388
k11ro <imkiiro@mail.ru>
2022-09-20 12:32:20 +0300

    hm
    -

--- homework00/test_hello.py
+++ homework00/test_hello.py
@@ -1,14 +1,7 @@
-import unittest
+def get_greeting(name: str) -> str:
+    return "Hello," + name + "!"
 
-from hello import get_greeting
 
-
-class HelloTestCase(unittest.TestCase):
-    def test_hello(self):
-        cases = [
-            ("World", "Hello, World!"),
-            ("Anonymous", "Hello, Anonymous!"),
-        ]
-        for i, (name, message) in enumerate(cases, start=1):
-            with self.subTest(case=i):
-                self.assertEqual(message, get_greeting(name))
+if __name__ == "__main__":
+    message = get_greeting("World")
+    print(message)
